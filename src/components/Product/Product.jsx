0604212b166bc04a2d6d818ee10bb3078a8f8562
rijwanulalam <rijwanul007@gmail.com>
2023-12-21 18:22:27 +0600

    shop component added and display all products

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 import "./Product.css";
 
 const Product = (props) => {
-    const {name, img, price, quantity, ratings, seller} = props.product;
-    return (
-        <div className='product'>
-            <img src={img} alt="" />
-            <h5>{name}</h5>
-            <p>Price: ${price}</p>
-        </div>
-    );
+  const { name, img, price, quantity, ratings, seller } = props.product;
+  return (
+    <div className="product">
+      <img src={img} alt="" />
+      <div className="product-info">
+        <h5 className="product-name">{name}</h5>
+        <p>Price: ${price}</p>
+        <p>Manufacturer: {seller}</p>
+        <p>Ratign: {ratings} stars</p>
+      </div>
+      <button className="btn-cart">Add To Cart</button>
+    </div>
+  );
 };
 
 export default Product;
