03ffb088f0fdc0d9f97c3464f689a997fbee090e
rijwanulalam <rijwanul007@gmail.com>
2023-12-27 20:41:23 +0600

    product quantity retrieve from the Local Storage and set it to the UI

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,16 +5,19 @@ const Cart = ({ cart }) => {
 
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
-        totalShipping = totalShipping + product.shipping;
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
     const tax = ((total * 7)/100);
     const grandTotal = total + totalShipping + tax;
   return (
     <div className="cart">
       <h3 className="summary">Order Summary</h3>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${total}</p>
       <p>Total Shipping Charge: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
